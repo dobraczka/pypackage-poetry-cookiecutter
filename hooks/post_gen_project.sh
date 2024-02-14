bf54aff806300d0d2aebba9a3eb26568838932c5
Daniel Obraczka <daniel@obraczka.de>
2024-02-14 13:48:47 +0100

    Initialize git as pre gen hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,5 @@
 
 poetry add --group dev ruff ipdb mypy pytest pytest-cov nox-poetry pytest-mock toml strawman pyroma
 poetry add --optional mkdocs mkdocs-material "mkdocstrings[python]" mkdocs-literate-nav mkdocs-gen-files mkdocs-section-index
+git init
 pre-commit install
